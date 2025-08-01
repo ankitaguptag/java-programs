ac175c0076d35b2107602acfb6f07d1dbaf884d8
Ankita Gupta <ankitagupta18205@gmail.com>
2025-08-01 16:32:55 +0530

    Sum program

---
+++ Basic/Classobject.java
@@ -0,0 +1,9 @@
+public class Classobject {
+  int x = 5;
+  int y = 10;
+  int sum = x+y;
+  public static void main(String[] args) {
+    Classobject myObj = new Classobject();
+    System.out.println( "sum of number "+ myObj.sum);
+  }
+}

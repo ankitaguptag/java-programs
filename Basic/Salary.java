94b95b88c72228b8be2991ef97bca5e33678a17e
Ankita Gupta <ankitagupta18205@gmail.com>
2025-08-01 15:23:29 +0530

    Employee info

---
+++ Basic/Salary.java
@@ -0,0 +1,11 @@
+public class Salary  {
+    public static void main(String[] args) {
+        String name = "Ankita";
+        int id = 99;
+        double salary = 10000;
+        System.out.println("Employee ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Basic salary: ₹" + salary);
+       
+    }
+}

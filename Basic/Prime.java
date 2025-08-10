a829117b3d58c989f3a1da29b8d76a9947584231
Ankita Gupta <ankitagupta18205@gmail.com>
2025-08-10 18:09:03 +0530

    Prime number program

---
+++ Basic/Prime.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Prime {
+    public static void main(String[] args) {
+        int i, n,count=0;
+   System.out.println("Enter the number:");
+   Scanner r=new Scanner(System.in);
+        n=r.nextInt();
+
+        for ( i = 1; i<=n; i++)
+ {
+            if (n % i == 0)
+ {
+                
+               count ++;
+            }
+        }
+
+        if (count==2)
+            System.out.println("prime number");
+        else
+            System.out.println(" Not Prime number");
+    }
+}

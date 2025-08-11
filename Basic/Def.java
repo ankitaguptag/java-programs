9c0e559b30d46b9bc65164eb2b90892133ab6842
Ankita Gupta <ankitagupta18205@gmail.com>
2025-08-11 17:10:02 +0530

    default program

---
+++ Basic/Def.java
@@ -0,0 +1,36 @@
+/* class A{
+int a;
+String b;
+boolean c;
+A(){
+a = 100;
+b = "ankita";
+c=true;
+}
+void Disp(){
+System.out.print(a+" "+b+" "+c);
+}
+}
+class Def{
+ public static void main(String[] args){
+A ref=new A();
+ref.Disp();
+}
+}*/
+
+                /* Default Constructor*/
+
+class A{
+int a;
+String b;
+boolean c;
+void Disp(){
+System.out.print(a+" "+b+" "+c);
+}
+}
+class Def{
+ public static void main(String[] args){
+A ref=new A();
+ref.Disp();
+}
+}

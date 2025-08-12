cf2a711797d8cf50a337c506bb54893aff0a45c5
Ankita Gupta <ankitagupta18205@gmail.com>
2025-08-12 14:54:17 +0530

    Palindrome program

--- Basic/Palindrome.java
+++ Basic/Palindrome.java
@@ -1,18 +1,21 @@
+import java.util.Scanner;
 public class Palindrome{
     public static void main(String[] args) {
-        int num = 121;  
-        int orig = num;
+        int num;  
         int rev = 0;
-
-        while (num > 0) {
+        System.out.println("Enter a number:");
+        Scanner r=new Scanner(System.in);
+        num=r.nextInt();
+        int orig = num;
+while (num > 0) {
             int digit = num % 10;
             rev = rev * 10 + digit;
             num = num / 10;
         }
-
-        if (orig == rev) {
+if (orig == rev) {
             System.out.println(orig + " is a Palindrome number.");
-        } else {
+ }
+ else {
             System.out.println(orig + " is NOT a Palindrome number.");
         }
     }

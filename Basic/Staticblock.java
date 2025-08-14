ff08a1b7e320df78a0cff5ffb1146f7ef6a640e1
Ankita Gupta <ankitagupta18205@gmail.com>
2025-08-14 12:20:17 +0530

    Static block program

---
+++ Basic/Staticblock.java
@@ -0,0 +1,14 @@
+class Staticblock{
+public static void main(String[] args){
+Staticblock r=new Staticblock();
+}
+Staticblock(){
+System.out.println("Please share");
+}
+{
+System.out.println("Please Like ");
+}
+static{
+System.out.println("I am Ankita");
+}
+}

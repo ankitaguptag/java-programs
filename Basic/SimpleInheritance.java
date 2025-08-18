3dc5002ea065510d54c514f95135bd9867d87f5b
Ankita Gupta <ankitagupta18205@gmail.com>
2025-08-18 11:56:07 +0530

    simple inheritance program

---
+++ Basic/SimpleInheritance.java
@@ -0,0 +1,19 @@
+class Student{
+protected int roll,marks;
+String name;
+protected void input(){
+System.out.println("Enter roll number & marks:");
+}
+}
+class SimpleInheritance extends Student
+{
+  void disp(){
+  roll=1; name="ankita"; marks=89;
+System.out.println(roll+" "+name+" "+marks);
+}
+public static void main(String[] args){
+SimpleInheritance r=new SimpleInheritance();
+r.input();
+r.disp();
+}
+}
